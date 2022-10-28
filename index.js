d1dc139791d32cbbf568cb113e9f2056d6c235c9
samahasama23@gmail.com <samahasama23@gmail.com>
2022-10-28 22:24:23 +0530

    button func

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+let array=[]
+let flag=false
 function buttonClick(val){
-    document.getElementById("screen").value+=val
+    if(flag){
+
+        document.getElementById("screen").value=val
+        flag=false
+    }else{
+        document.getElementById("screen").value+=val
+
+    }
+    
+
+}
+function operatorClick(val){
+
+    array.push(document.getElementById("screen").value+val)
+    document.getElementById("screen").value=val
+    flag=true
 }
 function clearDisplay(){
     document.getElementById("screen").value=""
+    array=[]
 }
 function equalClick(){
-    var text=document.getElementById("screen").value
+    var text=array.join("")+document.getElementById("screen").value;
     var result=eval(text)
     document.getElementById("screen").value=result
+    array=[]
     
 }
